60c5a4f2ad6b3c74b3838d082d63a69f6926a468
MichaelWijaya2509 <123831952+michaelwijaya2509@users.noreply.github.com>
2025-02-12 19:37:08 +0700

    Menambahkan fitur baru

---
+++ book.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 12, 2025 at 01:30 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `midproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `publisher` varchar(255) NOT NULL,
+  `number_of_page` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`id`, `name`, `author`, `publisher`, `number_of_page`, `user_id`) VALUES
+(1, 'java', 'java', 'binus', 11111, 66887),
+(2, 'family guy', 'jon', 'test', 1234, 66887);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2025 at 09:33 AM
+-- Generation Time: Feb 12, 2025 at 01:28 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,7 +38,13 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(77295, 'joni', '$2y$10$ReH2a9bLcZKkeFuvDD3l1uVBBs63/NEZg6Pu.2NVAwhY4.N.DzRFm');
+(5441, 'bob4', '$2y$10$LvYTe56dPe2a65Q8PNIKOuqfomjPJ2qTpdbNba6IkwnrUiL6wEFj6'),
+(23150, 'bob', '$2y$10$iBRh5Tnua.e32m6TYCbkJ.qvYXuPXyv8gQGOSpypuTb56nv7S9asK'),
+(25516, 'nic', '$2y$10$BplC17VJerYbEjuMuU2IL.ZV4s.d4KdqYisjRpNOF35H6C3mMCNmW'),
+(40693, 'toni', '$2y$10$jvOEszkf8U6DZ9KocqKqw./8ihXn9su.q25C0yxGg9jWHXWSZb3Fq'),
+(66887, 'hasta', '$2y$10$kVlaogLTZOGNm9Iwx2w4P.04uSzD8vbJJIzcGxuF9QNgwYngjCFVm'),
+(86235, 'test', '$2y$10$lMz5wQTxUkIb1BApnSOMyuOBfF5P5OCwPEYpp7qYrYdI8AjzVuo66'),
+(96040, 'test2', '$2y$10$D.4d4.d6fLLRGcdhVlXliut1tzgsYO1RAKt31KXgPtBrbIhjgPZIK');
 
 --
 -- Indexes for dumped tables
